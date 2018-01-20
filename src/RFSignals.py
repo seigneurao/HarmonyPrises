fa1b87be8c9a8e90f8bd8451c3b982927b31825d
SeigneurAo <seigneurao@gmail.com>
2018-01-20 23:41:12 +0100

    Change path

--- src/RFSignals.py
+++ src/RFSignals.py
@@ -7,7 +7,7 @@ Created on 20 janv. 2018
 def affich():
     title = 'Days of the Week\n'
 
-    new_path = 'D:/new_days.txt'
+    new_path = 'new_days.txt'
     new_days = open(new_path,'w')
     
     new_days.write(title)
