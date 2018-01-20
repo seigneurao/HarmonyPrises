bd8bbc52084502b6af266190b90ba7bb002ee715
SeigneurAo <seigneurao@gmail.com>
2018-01-20 19:58:11 +0100

    Lightware

---
+++ src/Lightwave.py
@@ -0,0 +1,20 @@
+'''
+Created on 20 janv. 2018
+
+@author: Lionel
+'''
+
+#!/usr/bin/python3
+
+aCommand = ""
+
+while aCommand != "0":
+    aCommand = input("Command? ")
+    if aCommand == "Q":
+        print("Haha")
+    elif aCommand == "2":
+        print("Héhé")
+    elif aCommand == "3":
+        print("Hoho")
+
+print("Finished!")
