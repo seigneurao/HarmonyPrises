a6c69947cf43121e29888222c21bd091e88e9ce4
SeigneurAo <seigneurao@gmail.com>
2018-01-21 11:46:48 +0100

    Adjust WS app params

--- src/SendRFSignals.py
+++ src/SendRFSignals.py
@@ -4,18 +4,18 @@ Created on 20 janv. 2018
 @author: Lionel
 '''
 
-from bottle import Bottle, run, template
+from bottle import Bottle
 from RFSignals import affich
 
 app = Bottle()
 
 @app.route('/hello/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return app.template('<b>Hello {{name}}</b>!', name=name)
 
 @app.route('/testlionel/<machin>/<truc>')
 def bidule(machin, truc):
-    return template('{{bonjour}} et {{what}}', bonjour=machin, what=truc)
+    return app.template('{{bonjour}} et {{what}}', bonjour=machin, what=truc)
 
 @app.route('/affichage')
 def affichage():
@@ -24,4 +24,4 @@ def affichage():
 def get_app():
     return app
 
-run(app, host='localhost', port=8080)
+app.run()
