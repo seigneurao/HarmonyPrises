68f48b9cc3270a2099d115cedc94b128ee6aa83b
SeigneurAo <seigneurao@gmail.com>
2018-01-26 22:45:34 +0100

    Generic keystroke read

--- src/CatchKeystrokes.py
+++ src/CatchKeystrokes.py
@@ -17,21 +17,17 @@ KEY_DOWN = 1
 # Held
 KEY_HOLD = 2
 
-OUTLET1_ON_KEY = "KEY_1"
-OUTLET1_OFF_KEY = "KEY_2"
-OUTLET2_ON_KEY = "KEY_3"
-OUTLET2_OFF_KEY = "KEY_4"
+keyMapping = {}
+keyMapping["KEY_1"] = "switchOnOutlet1"
+keyMapping["KEY_2"] = "switchOffOutlet1"
+keyMapping["KEY_3"] = "switchOnOutlet2"
+keyMapping["KEY_4"] = "switchOffOutlet2"
+
 
 for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
         keyEvent = evdev.categorize(event)
         if keyEvent.keystate == KEY_DOWN:
             keyCode = keyEvent.keycode
-            if keyCode == OUTLET1_ON_KEY:
-                RFSignals.switchOnOutlet1()
-            elif keyCode == OUTLET1_OFF_KEY:
-                RFSignals.switchOffOutlet1()
-            elif keyCode == OUTLET2_ON_KEY:
-                RFSignals.switchOnOutlet2()
-            elif keyCode == OUTLET2_OFF_KEY:
-                RFSignals.switchOffOutlet2()
+            targetMethod = getattr(RFSignals, keyMapping[keyCode])
+            targetMethod()
